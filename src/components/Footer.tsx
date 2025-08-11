7064f631a2c6a8a399b3d82c282d5a2a93b70416
Sreenivas7463 <sreenivasadyourworld@gmail.com>
2025-08-11 21:00:41 +0530

    up

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="py-12 text-center text-gray-700 bg-gray-100">
+    <footer className="py-12 text-center text-white bg-gray-800">
       <p>&copy; {new Date().getFullYear()} Gethu Cinema. All rights reserved.</p>
     </footer>
   );
